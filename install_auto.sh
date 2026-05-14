715c931d53eb96349a0df873b4fdac26d4f30f95
lzhenwei <lzhenwei76@gmail.com>
2026-05-14 13:42:12 +0200

    Create install_auto.sh
    Run this Bash code to automatically install the Opiumware with the correct architecturect

---
+++ install_auto.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+if [[ "$ARCH" == "arm64" ]]; then
+    echo "Apple Silicon (ARM64) erkannt"
+    bash <(curl -fsSL https://raw.githubusercontent.com/lzhenweiDev/Opiumware-Installer/refs/heads/main/install_arm64.sh)
+
+elif [[ "$ARCH" == "x86_64" ]]; then
+    echo "Intel Mac erkannt"
+    bash <(curl -fsSL https://raw.githubusercontent.com/lzhenweiDev/Opiumware-Installer/refs/heads/main/install_intel.sh)
+
+else
+    echo "Nicht unterstützte Architektur: $ARCH"
+    exit 1
+fi
