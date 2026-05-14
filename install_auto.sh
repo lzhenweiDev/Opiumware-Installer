a49fb868e19ee3af5a744c3bc6d6876f9559a818
lzhenwei <lzhenwei76@gmail.com>
2026-05-14 13:47:21 +0200

    Update install_auto.sh

--- install_auto.sh
+++ install_auto.sh
@@ -3,14 +3,14 @@
 ARCH=$(uname -m)
 
 if [[ "$ARCH" == "arm64" ]]; then
-    echo "Apple Silicon (ARM64) erkannt"
+    echo "Apple Silicon (ARM64) detected"
     bash <(curl -fsSL https://raw.githubusercontent.com/lzhenweiDev/Opiumware-Installer/refs/heads/main/install_arm64.sh)
 
 elif [[ "$ARCH" == "x86_64" ]]; then
-    echo "Intel Mac erkannt"
+    echo "Intel Mac detected"
     bash <(curl -fsSL https://raw.githubusercontent.com/lzhenweiDev/Opiumware-Installer/refs/heads/main/install_intel.sh)
 
 else
-    echo "Nicht unterstützte Architektur: $ARCH"
+    echo "Architecture not supported $ARCH"
     exit 1
 fi
